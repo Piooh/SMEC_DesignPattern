c48f4fa3574795a8275818b41cca92f8c4ef40e3
Pioh <piohsmec@gmail.com>
2016-09-17 22:02:43 +0900

    Design Pattern strategy #1

---
+++ Assets/Lecture3/Character.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using ReadOnlys;
+
+public class Character : MonoBehaviour
+{
+	public Animator animator = null;
+
+	private void Awake()
+	{
+		Debug.Assert( null != animator, "Check Animator!!!!" );
+	}
+
+	private void FixedUpdate()
+	{
+		if( true == Input.GetMouseButtonUp(0) )
+		{
+			Attack();
+		}
+	}
+
+	private void Attack()
+	{
+		if( animator.GetCurrentAnimatorStateInfo(0).fullPathHash != Lecture3.AnimationID.SwordWait )
+		{
+			return;
+		}
+
+		Debug.LogError("Attttttttttttttttttttttack");
+		animator.SetInteger( Lecture3.AnimationID.SwordState, Lecture3.Action.Attack.ToInt() );
+
+		StartCoroutine( CheckAniEnd() );
+	}
+
+	private IEnumerator CheckAniEnd()
+	{
+		float playTime = 0f;
+		float endTime = animator.GetCurrentAnimatorStateInfo(0).length;
+
+		do
+		{
+			playTime  += 0.1f;
+			yield return null;
+		}
+		while( playTime > endTime );
+
+		animator.SetInteger( Lecture3.AnimationID.SwordState, Lecture3.Action.Idle.ToInt() );
+	}
+}
---
+++ Assets/Lecture3/ReadOnlys.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace ReadOnlys
+{
+	public static class Lecture3
+	{
+		public enum Action 
+		{
+			Idle = 0,
+			Attack = 1,
+		}
+
+		public class AnimationID
+		{
+			public static readonly int SwordState		= Animator.StringToHash("AnimationState");
+			public static readonly int SwordWait		= Animator.StringToHash("Base Layer.Wait");
+			public static readonly int SwordAttack		= Animator.StringToHash("Base Layer.Attack");
+		}
+
+		public static int ToInt( this Action action )
+		{
+			switch( action )
+			{
+				case Action.Attack:	return 1;
+
+				case Action.Idle:
+				default:						return 0;
+			}
+		}
+	}
+}
